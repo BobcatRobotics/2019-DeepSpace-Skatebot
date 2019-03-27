fc62237eadf20bd8558bd9db4b3a5261d6257e10
BobCat177 <bobcatrobotics@gmail.com>
2019-03-27 19:49:54 -0400

    Added integral for driving

--- src/main/java/frc/robot/commands/TargetBot.java
+++ src/main/java/frc/robot/commands/TargetBot.java
@@ -13,15 +13,20 @@ import frc.robot.lib.RioLogger;
 import frc.robot.lib.RioLoggerThread;
 
 public class TargetBot extends Command {
-	private static double DESIRED_TARGET_AREA = 17; // Area of the target when the robot reaches the wall
-	private static double DRIVE_K = 0.045; // how hard to drive fwd toward the target
-	private static double STEER_K = 0.015; // how hard to turn toward the target
-	private static double X_OFFSET = 4.5;  // The number of degrees camera is off center
+	private static double DESIRED_TARGET_AREA = 16.4; // Area of the target when the robot reaches the wall
+	private static double DRIVE_K = 0.020; // how hard to drive fwd toward the target
+	private static double DRIVE_I = 0.08; //tune integral
+	private static double STEER_K = 0.010; //0.015; // how hard to turn toward the target
+	private static double X_OFFSET = -10.4;  // The number of degrees camera is off center
+	private static double dt = .02; 
 
 	// The following fields are updated by the LimeLight Camera
 	private boolean hasValidTarget = false;
 	private double driveCommand = 0.0;
 	private double steerCommand = 0.0;
+	private double driveIntegral = 0.0;
+	private double steerIntegral = 0.0;
+
 
 	// The following fields are updated by the state of the Command
 	private boolean ledsON = false;
@@ -52,12 +57,12 @@ public class TargetBot extends Command {
 
 		double leftPwr = (driveCommand - steerCommand) * -1.0;
 		double rightPwr = (driveCommand + steerCommand) * -1.0;
-		if (Math.abs(ta0 - ta1) > 0.05) {
-			if (ta0 > ta1)
-				rightPwr += 0.25;
-			else
-				leftPwr += 0.25;
-		}
+		// if (Math.abs(ta0 - ta1) > 0.05) {
+		// 	if (ta0 > ta1)
+		// 		rightPwr += 0.25;
+		// 	else
+		// 		leftPwr += 0.25;
+		// }
 	
 		OI.driveTrain.setLeftPower(leftPwr);
 		OI.driveTrain.setRightPower(rightPwr);
@@ -83,6 +88,9 @@ public class TargetBot extends Command {
 		// }
 		// }
 		if (OI.gamePad.getRawButton(3)) {
+			stop = true;
+		}
+		if(OI.limelight.targetArea() > DESIRED_TARGET_AREA){
 			stop = true;
 		}
 		return stop;
@@ -120,11 +128,21 @@ public class TargetBot extends Command {
 		log.ts1 = OI.limelight.targetSkew(1);
 
 		// Start with proportional steering
+		
 		steerCommand = (tx - X_OFFSET) * STEER_K;
 		SmartDashboard.putNumber("Limelight.SteerCommand", steerCommand);
 
 		// try to drive forward until the target area reaches our desired area
-		driveCommand = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+		double distanceError = DESIRED_TARGET_AREA - ta;
+		if( Math.abs(distanceError) < 2){
+			driveIntegral = driveIntegral + dt*distanceError;
+		}
+		else{
+			driveIntegral = 0.0;
+		}
+		RioLogger.errorLog("Drive Integral:" + driveIntegral + "Actual: " + DRIVE_K*distanceError);
+		RioLogger.errorLog("Drive Error:" + driveIntegral + "Actual Drive Error: " + driveIntegral * DRIVE_I);
+		driveCommand = DRIVE_K*distanceError + driveIntegral * DRIVE_I;
 		SmartDashboard.putNumber("Limelight.DriveCommand", driveCommand);
 
 		log.drvCmd = driveCommand;
